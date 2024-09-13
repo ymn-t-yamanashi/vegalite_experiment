e06fcc266cdfcca881fe286f7584e0f80c1fe1a2
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-13 17:40:15 +0900

    mix new vegalite_experiment

---
+++ lib/vegalite_experiment.ex
@@ -0,0 +1,18 @@
+defmodule VegaliteExperiment do
+  @moduledoc """
+  Documentation for `VegaliteExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VegaliteExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vegalite_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule VegaliteExperimentTest do
+  use ExUnit.Case
+  doctest VegaliteExperiment
+
+  test "greets the world" do
+    assert VegaliteExperiment.hello() == :world
+  end
+end
