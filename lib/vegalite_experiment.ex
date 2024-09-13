7a8211d6433420445e29bf5152f4cec9c4db0eea
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-13 18:25:46 +0900

    サンプル作成

--- lib/vegalite_experiment.ex
+++ lib/vegalite_experiment.ex
@@ -2,6 +2,7 @@ defmodule VegaliteExperiment do
   @moduledoc """
   Documentation for `VegaliteExperiment`.
   """
+  alias VegaLite, as: Vl
 
   @doc """
   Hello world.
@@ -12,7 +13,18 @@ defmodule VegaliteExperiment do
       :world
 
   """
+
   def hello do
+    y = for _ <- 1..20, do:  Enum.random(1..100)
+
+    Vl.new(width: 400, height: 400)
+    |> Vl.data_from_values(x: 1..20, y: y)
+    |> Vl.mark(:bar)
+    |> Vl.encode_field(:x, "x", type: :quantitative)
+    |> Vl.encode_field(:y, "y", type: :quantitative)
+    |> Vl.Export.to_png()
+    |> then(& File.write!("test.png", &1))
+
     :world
   end
 end
