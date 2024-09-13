a1e74a77b3bf4dedee76e6ddcb7d1bded4111197
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-13 19:18:19 +0900

    色と幅を変更

--- lib/vegalite_experiment.ex
+++ lib/vegalite_experiment.ex
@@ -17,16 +17,16 @@ defmodule VegaliteExperiment do
   def hello do
     y = for _ <- 1..20, do: Enum.random(1..100)
 
-    Vl.new()
+    Vl.new(width: 800, height: 400)
     |> Vl.data_from_values(x: 1..20, y: y)
     |> Vl.layers([
-      Vl.new(width: 400, height: 400)
-      |> Vl.mark(:bar)
+      Vl.new()
+      |> Vl.mark(:bar, color: "#ffaaaa", width: 20)
       |> Vl.encode_field(:x, "x", type: :quantitative)
       |> Vl.encode_field(:y, "y", type: :quantitative),
-      Vl.new(width: 400, height: 400)
-      |> Vl.mark(:line)
-      |> Vl.encode_field(:x, "x", type: :quantitativ)
+      Vl.new()
+      |> Vl.mark(:line, color: "#aaaaff")
+      |> Vl.encode_field(:x, "x", type: :quantitative)
       |> Vl.encode_field(:y, "y", type: :quantitative)
     ])
     |> Vl.Export.to_png()
