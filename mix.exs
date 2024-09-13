4ba0d9c23fbae6132725ce04ae1132836dd450e4
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-13 17:44:39 +0900

    depsにvega_lite追加

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule VegaliteExperiment.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:vega_lite, "~> 0.1.9"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
